dc04b8f0eb0d46f0ce76c995eee3bb21bb1ddcfb
Alexandru Gyori <alexandru.gyori@studentpartner.com>
2012-12-05 19:27:33 +0200

    Made a concrete NewsPublisher class that sends news to JMS.

--- NewsPublisher/src/ro/upt/pcbe/newspublisher/NewsPublisher.java
+++ NewsPublisher/src/ro/upt/pcbe/newspublisher/NewsPublisher.java
@@ -1,9 +1,56 @@
 package ro.upt.pcbe.newspublisher;
 
-public class NewsPublisher {
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
-	public NewsPublisher(String topicName, String username, String Password)
+import javax.jms.JMSException;
+import javax.jms.TopicPublisher;
+import javax.naming.NamingException;
+
+import ro.upt.pcbe.jmshelpers.Publisher;
+
+public class NewsPublisher extends Publisher{
+
+	
+	public NewsPublisher(String topicName, String username, String password) throws NamingException, JMSException
 	{
+		super(topicName, username, password);
+	}
+	
+	@Override
+	public void run()
+	{
+		Random r = new Random();
+		while(true)
+		{
+			try {
+				this.publish(this.makeHm());
+			} catch (JMSException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			try {
+				Thread.sleep(r.nextInt(10000));
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 	}
+	
+	public Map<String, String> makeHm()
+	{
+		Map<String,String> hm = new HashMap<String, String>();
+		hm.put("domain", "Sport");
+		hm.put("subdomain", "Tenis");
+		hm.put("creationDate", new Date().toString());
+		hm.put("lastModificationDate", new Date().toString());
+		hm.put("link", "http://hotnews.ro");
+		return hm;
+	}
+	
 }
