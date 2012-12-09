04b74d3398d6a7da0b38293f4bf49cbb4957b523
shaman203 <matyi204@gmail.com>
2012-12-09 19:26:31 +0200

    added subscriber father class

---
+++ JMSHelpers/src/ro/upt/pcbe/jmshelpers/Subscriber.java
@@ -0,0 +1,71 @@
+package ro.upt.pcbe.jmshelpers;
+
+import javax.jms.*;
+import javax.naming.*;
+import java.io.*;
+import java.util.Properties;
+
+public class Subscriber implements javax.jms.MessageListener {
+	private TopicSession subSession;
+	private TopicConnection connection;
+
+	/* Constructor. Establish JMS publisher and subscriber */
+	public Subscriber(String topicName, String username, String password)
+			throws Exception {
+		// Obtain a JNDI connection
+		Properties env = new Properties();
+		// ... specify the JNDI properties specific to the vendor
+
+		InitialContext jndi = new InitialContext(env);
+
+		// Look up a JMS connection factory
+
+		TopicConnectionFactory conFactory = (TopicConnectionFactory) jndi .lookup("TopicConnectionFactory");
+
+		// Create a JMS connection
+		TopicConnection connection = conFactory.createTopicConnection(username, password);
+
+		// Create JMS session object
+		TopicSession subSession = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		// Look up a JMS topic
+		Topic topic = (Topic) jndi.lookup(topicName);
+
+		// Create a JMS subscriber
+		TopicSubscriber subscriber = subSession.createSubscriber(topic);
+
+		// Set a JMS message listener
+		subscriber.setMessageListener(this);
+
+		set(subSession, connection);
+		
+		// Start the JMS connection; allows messages to be delivered
+		connection.start();
+
+	}
+
+	private void set(TopicSession subSession, TopicConnection connection) {
+		// TODO Auto-generated method stub
+		this.connection = connection;
+		this.subSession = subSession;
+	}
+
+	/* Receive message from topic subscriber */
+	public void onMessage(Message message) {
+		try {
+			TextMessage textMessage = (TextMessage) message;
+			String text = textMessage.getText();
+			System.out.println(text);
+		} catch (JMSException jmse) {
+			jmse.printStackTrace();
+		}
+	}
+
+	
+	/* Close the JMS connection */
+	public void close() throws JMSException {
+		connection.close();
+	}
+}
+
+
