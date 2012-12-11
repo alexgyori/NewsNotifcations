d3e4725caf5bb99008a3336db0345d493f43b166
Alexandru Gyori <alexandru.gyori@studentpartner.com>
2012-12-11 15:43:59 +0200

    More fine grained concurrency.

--- NewsPublisher/src/ro/upt/pcbe/newspublisher/ReadNotificationsSubscriber.java
+++ NewsPublisher/src/ro/upt/pcbe/newspublisher/ReadNotificationsSubscriber.java
@@ -7,33 +7,45 @@ import java.util.concurrent.ConcurrentHashMap;
 import javax.jms.JMSException;
 import javax.jms.Message;
 
+import ro.upt.pcbe.jmshelpers.Subscriber;
 
 
-public class ReadNotificationsSubscriber  implements javax.jms.MessageListener{
 
+public class ReadNotificationsSubscriber  extends Subscriber{
+
+	public ReadNotificationsSubscriber(String topicName, String username, String password) throws Exception
+	{
+		super(topicName, username,password);
+	}
+	
 	private Map<String, Integer> newsToNumberOfReads = new ConcurrentHashMap<String, Integer>();
 	@Override
 	//synchronized on the whole method because it needs to be atomic
 	//because of jms this method gets called concurrently
-	public synchronized void onMessage(Message arg0) {
+	public void onMessage(Message arg0) {
 		try {
 			String newsId = arg0.getStringProperty("newsId");
 			//Check if already received notifications for the current news id
 			//if not add a new entry with number of occurances 1.
 			Integer count = 0;
-			if(this.newsToNumberOfReads.containsKey(newsId))
+			synchronized(this)
 			{
-				count = this.newsToNumberOfReads.get(newsId);
-				
-			}			
-			
-			this.newsToNumberOfReads.put(newsId, count+1);
-			
+				if(this.newsToNumberOfReads.containsKey(newsId))
+				{
+					count = this.newsToNumberOfReads.get(newsId);					
+				}							
+				this.newsToNumberOfReads.put(newsId, count+1);
+			}
 		} catch (JMSException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+	}
+	@Override
+	protected String getSelectorString() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
