35338e77c76eed258c6da7c58983367e685161fc
shaman203 <matyi204@gmail.com>
2012-12-12 10:51:42 +0200

    added hashcode, equals, toString

--- JMSHelpers/src/ro/upt/pcbe/jmshelpers/DoSub.java
+++ JMSHelpers/src/ro/upt/pcbe/jmshelpers/DoSub.java
@@ -1,6 +1,8 @@
 package ro.upt.pcbe.jmshelpers;
 
-public class DoSub {
+import java.io.Serializable;
+
+public class DoSub implements Serializable{
 
 	
 	public String domain;
@@ -11,4 +13,43 @@ public class DoSub {
 		this.domain = domain;
 		this.subdomain = subdomain;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((domain == null) ? 0 : domain.hashCode());
+		result = prime * result
+				+ ((subdomain == null) ? 0 : subdomain.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DoSub other = (DoSub) obj;
+		if (domain == null) {
+			if (other.domain != null)
+				return false;
+		} else if (!domain.equals(other.domain))
+			return false;
+		if (subdomain == null) {
+			if (other.subdomain != null)
+				return false;
+		} else if (!subdomain.equals(other.subdomain))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "DoSub [domain=" + domain + ", subdomain=" + subdomain + "]";
+	}
+	
+	
 }
